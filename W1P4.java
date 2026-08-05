0000bdb3a8761467b9a3c63c9330c49b0ade82cc
anandjyotsna1711-cmd <anand.jyotsna1711@gmail.com>
2026-08-05 15:06:32 +0530

    Add files via upload

---
+++ W1P4.java
@@ -0,0 +1,48 @@
+public class W1P4 {
+
+    static void analyzeInventory(int[] sectionA, int[] sectionB) {
+
+        int sumA = 0, sumB = 0;
+
+        int max = sectionA[0];
+        String section = "Section A";
+        int index = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+
+            sumA += sectionA[i];
+            sumB += sectionB[i];
+
+            if (sectionA[i] > max) {
+                max = sectionA[i];
+                section = "Section A";
+                index = i;
+            }
+
+            if (sectionB[i] > max) {
+                max = sectionB[i];
+                section = "Section B";
+                index = i;
+            }
+        }
+
+        System.out.println("Section A Total: " + sumA);
+        System.out.println("Section B Total: " + sumB);
+
+        if (sumA == sumB)
+            System.out.println("Status: Balanced");
+        else
+            System.out.println("Status: Not Balanced");
+
+        System.out.println("Highest Quantity: " + max +
+                " (" + section + ", Item " + (index + 1) + ")");
+    }
+
+    public static void main(String[] args) {
+
+        int[] sectionA = {20, 15, 30};
+        int[] sectionB = {25, 10, 30};
+
+        analyzeInventory(sectionA, sectionB);
+    }
+}
